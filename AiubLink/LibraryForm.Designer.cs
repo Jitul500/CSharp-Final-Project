23470c7499dde4cba0cbbc4656aec48098fe0786
maishaisrat9 <misrat480@gmail.com>
2025-02-02 11:32:55 +0600

    Library

---
+++ AiubLink/LibraryForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace LibrarySystem
+{
+    partial class LibraryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold);
+            this.btnUpload.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnUpload.Location = new System.Drawing.Point(243, 393);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(98, 49);
+            this.btnUpload.TabIndex = 18;
+            this.btnUpload.Text = "Upload Book";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(426, 85);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(510, 398);
+            this.dataGridView.TabIndex = 17;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold);
+            this.txtGenre.Location = new System.Drawing.Point(223, 287);
+            this.txtGenre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(169, 24);
+            this.txtGenre.TabIndex = 16;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold);
+            this.txtAuthor.Location = new System.Drawing.Point(224, 230);
+            this.txtAuthor.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(168, 24);
+            this.txtAuthor.TabIndex = 15;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(223, 168);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(169, 24);
+            this.txtTitle.TabIndex = 14;
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblGenre.Location = new System.Drawing.Point(134, 287);
+            this.lblGenre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(59, 20);
+            this.lblGenre.TabIndex = 13;
+            this.lblGenre.Text = "Genre\r\n";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblAuthor.Location = new System.Drawing.Point(134, 230);
+            this.lblAuthor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(63, 20);
+            this.lblAuthor.TabIndex = 12;
+            this.lblAuthor.Text = "Author";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(134, 170);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(43, 20);
+            this.lblTitle.TabIndex = 11;
+            this.lblTitle.Text = "Title";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel1.Location = new System.Drawing.Point(-7, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(960, 74);
+            this.panel1.TabIndex = 19;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.DarkOrange;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(-7, 60);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(112, 489);
+            this.flowLayoutPanel1.TabIndex = 20;
+            // 
+            // LibraryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 547);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "LibraryForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LibraryForm";
+            this.Load += new System.EventHandler(this.LibraryForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ AiubLink/LibraryForm.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace LibrarySystem
+{
+    public partial class LibraryForm : Form
+    {
+        private string jsonFilePath = "books.json";  // JSON file to store book records
+        private string booksFolder = "Books";
+        public LibraryForm()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        private void LibraryForm_Load(object sender, EventArgs e)
+        {
+            LoadBooks();
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Configure DataGridView columns
+            dataGridView.Columns.Clear();
+            dataGridView.AutoGenerateColumns = false;
+            dataGridView.AllowUserToAddRows = false; // Disable placeholder row
+
+            dataGridView.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Title", HeaderText = "Title" });
+            dataGridView.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Author", HeaderText = "Author" });
+            dataGridView.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Genre", HeaderText = "Genre" });
+
+            // Add Open button column
+            var openButtonColumn = new DataGridViewButtonColumn
+            {
+                Name = "OpenButton",
+                HeaderText = "Open",
+                Text = "Open File",
+                UseColumnTextForButtonValue = true
+            };
+            dataGridView.Columns.Add(openButtonColumn);
+
+            // Add Delete button column
+            var deleteButtonColumn = new DataGridViewButtonColumn
+            {
+                Name = "DeleteButton",
+                HeaderText = "Delete",
+                Text = "Delete",
+                UseColumnTextForButtonValue = true
+            };
+            dataGridView.Columns.Add(deleteButtonColumn);
+
+            dataGridView.CellClick += DataGridView_CellClick;
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Files|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                UploadBook(txtTitle.Text, txtAuthor.Text, txtGenre.Text, filePath);
+                LoadBooks();
+            }
+        }
+
+        private void UploadBook(string title, string author, string genre, string filePath)
+        {
+            if (!Directory.Exists(booksFolder))
+                Directory.CreateDirectory(booksFolder);
+
+            if (!File.Exists(jsonFilePath))
+                File.WriteAllText(jsonFilePath, "[]");
+
+            string destPath = Path.Combine(booksFolder, Path.GetFileName(filePath));
+            File.Copy(filePath, destPath, true);
+
+            List<Book> books = GetBooks();
+            books.Add(new Book { Title = title, Author = author, Genre = genre, FilePath = destPath });
+
+            File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(books, Formatting.Indented));
+        }
+
+        private List<Book> GetBooks()
+        {
+            if (!File.Exists(jsonFilePath)) return new List<Book>();
+
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            return JsonConvert.DeserializeObject<List<Book>>(jsonContent) ?? new List<Book>();
+        }
+
+        private void LoadBooks()
+        {
+            dataGridView.DataSource = null;
+            dataGridView.DataSource = GetBooks();
+            dataGridView.Refresh(); // Ensure the DataGridView is refreshed
+        }
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView.Rows.Count) // Ensure valid row
+            {
+                var books = GetBooks();
+                var selectedBook = books[e.RowIndex];
+
+                if (e.ColumnIndex == dataGridView.Columns["OpenButton"].Index)
+                {
+                    OpenBookFile(selectedBook.FilePath);
+                }
+                else if (e.ColumnIndex == dataGridView.Columns["DeleteButton"].Index)
+                {
+                    DeleteBookRecord(selectedBook);
+                }
+            }
+        }
+
+        private void OpenBookFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                System.Diagnostics.Process.Start(filePath);
+            }
+            else
+            {
+                MessageBox.Show("File not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DeleteBookRecord(Book book)
+        {
+            var books = GetBooks();
+            var bookToRemove = books.Find(b => b.Title == book.Title && b.Author == book.Author && b.FilePath == book.FilePath);
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+
+                // Delete the file if it exists
+                if (File.Exists(book.FilePath))
+                {
+                    File.Delete(book.FilePath);
+                }
+
+                File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(books, Formatting.Indented));
+                LoadBooks(); // Refresh DataGridView after deletion
+
+                MessageBox.Show("Book record deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Book record not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string FilePath { get; set; }
+    }
+}
+    
+
